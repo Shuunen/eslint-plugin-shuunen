ac1197e236d51629a90dbcebc85bf4e107880be3
Romain Racamier-Lafon <romain.racamier@gmail.com>
2024-06-23 13:12:16 +0200

    bring back biome as formatter

--- rules/utils/objects.js
+++ rules/utils/objects.js
@@ -2,14 +2,14 @@
  * @param {object | null} object
  * @param {number} maxLevel
  */
-function capNesting (object, maxLevel) {
+function capNesting(object, maxLevel) {
   const one = 1
   /**
    * @param {object | null} currentObject
    * @param {number} currentLevel
    */
-  function capHelper (currentObject, currentLevel) {
-    if (currentLevel > maxLevel) return "..."
+  function capHelper(currentObject, currentLevel) {
+    if (currentLevel > maxLevel) return '...'
     if (typeof currentObject !== 'object' || currentObject === null) return currentObject
     const result = Array.isArray(currentObject) ? [] : {} // @ts-expect-error
     for (const key in currentObject) if (Object.hasOwn(currentObject, key)) result[key] = capHelper(currentObject[key], currentLevel + one)
